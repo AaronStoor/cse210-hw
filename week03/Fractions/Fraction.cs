66a6bcb3455dc987da14a3a7de9cc4e52b898d71
AaronStoor <Aaronbstoor@gmail.com>
2025-01-22 23:54:25 -0700

    Completed the program part of fractions

--- week03/Fractions/Fraction.cs
+++ week03/Fractions/Fraction.cs
@@ -10,20 +10,20 @@ public class Fraction
         _top = 1;
         _bottom = 1;
     }
-    public Fraction(wholeNumber int)
+    public Fraction(int wholeNumber)
     {
         _top = wholeNumber;
         _bottom = 1;
     }
-    public Fraction(top int, bottom int)
+    public Fraction(int top, int bottom)
     {
         _top = top;
         _bottom = bottom;
     }
     public string GetFractionString()
     {
-        string text = $"{_top}/{bottom}";
-        retrun text;
+        string text = $"{_top}/{_bottom}";
+        return text;
     }
     public double GetDecimalValue()
     {
