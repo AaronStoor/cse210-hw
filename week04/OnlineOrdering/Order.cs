654fcbf69a8384c496f9c8439ca17d2cf633f08a
AaronStoor <Aaronbstoor@gmail.com>
2025-01-30 00:34:31 -0700

    added logic to order class

--- week04/OnlineOrdering/Order.cs
+++ week04/OnlineOrdering/Order.cs
@@ -2,5 +2,39 @@ using System;
 
 public class Order()
 {
-    
+    private List<Product> products;
+    private Customer customer;
+
+    public Order(List<Product> products, Customer customer)
+    {
+        this.products = products;
+        this.customer = customer;
+    }
+
+    public double TotalCost()
+    {
+        double productTotal = 0;
+        foreach (var product in products)
+        {
+            productTotal += product.TotalCost();
+        }
+
+        double shippingCost = customer.LivesInUSA() ? 5 : 35;
+        return productTotal + shippingCost;
+    }
+
+    public string PackingLabel()
+    {
+        string label = "";
+        foreach (var product in products)
+        {
+            label += $"{product.Name} (ID: {product.ProductId})\n";
+        }
+        return label.TrimEnd();
+    }
+
+    public string ShippingLabel()
+    {
+        return $"{customer.Name}\n{customer.Address.FullAddress()}";
+    }
 }
