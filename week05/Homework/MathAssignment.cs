5ffb07153d261a6b753f645a183a2707ce0a78f3
AaronStoor <Aaronbstoor@gmail.com>
2025-02-05 23:43:05 -0700

    Completed the assignment

--- week05/Homework/MathAssignment.cs
+++ week05/Homework/MathAssignment.cs
@@ -2,8 +2,8 @@ public class MathAssignment : Assignment
 {
     private string _textbookSection;
     private string _problems;
-
-    public MathAssignment(string studentName, string topic, string textbookSection, string problems) 
+    
+    public MathAssignment(string studentName, string topic, string textbookSection, string problems)
         : base(studentName, topic)
     {
         _textbookSection = textbookSection;
